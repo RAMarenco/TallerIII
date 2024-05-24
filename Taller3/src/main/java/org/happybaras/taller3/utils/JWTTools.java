8a2f9d57b73604ccb0f3b5636e97eb0b5a5f1631
RAMarenco <00080121@uca.edu.sv>
2024-05-23 18:46:22 -0600

    fix import

--- Taller3/src/main/java/org/happybaras/taller3/utils/JWTTools.java
+++ Taller3/src/main/java/org/happybaras/taller3/utils/JWTTools.java
@@ -1,13 +1,13 @@
 package org.happybaras.taller3.utils;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JWTTools {
-    @Value("${jwt.secret}")
+    @Value("${security.jwt.secret}")
     private String secret;
 
-    @Value("${jwt.exptime}")
+    @Value("${security.jwt.exptime}")
     private Integer exp;
 }
