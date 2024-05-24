e53fc891f182f8800ad7b1ded18b724346e74338
mrivasc <00015121@uca.edu.sv>
2024-05-23 19:21:41 -0600

    🤡 add: userRepository and updated UserServiceImpl

--- Taller3/src/main/java/org/happybaras/taller3/services/UserService.java
+++ Taller3/src/main/java/org/happybaras/taller3/services/UserService.java
@@ -6,6 +6,10 @@ import org.happybaras.taller3.domain.entities.User;
 public interface UserService {
     // Token management
     Token registerToken(User user) throws Exception;
+
     Boolean isTokenValid(User user, String token);
+
     void cleanTokens(User user) throws Exception;
+
+    User findUserAuthenticated() throws Exception;
 }
