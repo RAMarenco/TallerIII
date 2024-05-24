b26401f4c4e1f86a83d69c5af4aa04748cab47ac
MonicaCabanas23 <00079921@uca.edu.sv>
2024-05-23 19:32:24 -0600

    Add register function

--- Taller3/src/main/java/org/happybaras/taller3/controllers/AuthController.java
+++ Taller3/src/main/java/org/happybaras/taller3/controllers/AuthController.java
@@ -2,10 +2,14 @@ package org.happybaras.taller3.controllers;
 
 import jakarta.validation.Valid;
 import org.happybaras.taller3.domain.dtos.GeneralResponse;
+import org.happybaras.taller3.domain.dtos.TokenDTO;
 import org.happybaras.taller3.domain.dtos.UserLoginDTO;
 import org.happybaras.taller3.domain.dtos.UserRegisterDTO;
+import org.happybaras.taller3.domain.entities.Token;
 import org.happybaras.taller3.domain.entities.User;
+import org.happybaras.taller3.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,13 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    //@Autowired
-    //private UserService userService;
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/login")
     public ResponseEntity<GeneralResponse> login(@ModelAttribute @Valid UserLoginDTO info) {
-        // Token token = userService.registerToken(user);
-        // return new ResponseEntity<>(new TokenDTO(token), HttpStatus.OK);
-        return GeneralResponse.builder().getResponse(); // Esto hay que quitarlo después
+
+        Token token = userService.registerToken(info);
+        return GeneralResponse.builder().status(HttpStatus.OK).data(token).getResponse();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<GeneralResponse> register(@ModelAttribute @Valid UserRegisterDTO info) {
+        User user = userService.findByUsernameOrEmail(info.getEmail(), info.getUsername());
+
+        if(user != null)
+            return GeneralResponse.builder().status(HttpStatus.CONFLICT).getResponse();
+
+        return GeneralResponse.builder().status(HttpStatus.OK).data(info).message("User registered succesfuly").getResponse();
     }
 }
