41bd2ba76fe0f541acc64e37a9e0c4f27ba58f53
MonicaCabanas23 <00079921@uca.edu.sv>
2024-05-23 19:13:11 -0600

    :sparkles: Add auth controller

--- Taller3/src/main/java/org/happybaras/taller3/controllers/AuthController.java
+++ Taller3/src/main/java/org/happybaras/taller3/controllers/AuthController.java
@@ -1,4 +1,27 @@
 package org.happybaras.taller3.controllers;
 
+import jakarta.validation.Valid;
+import org.happybaras.taller3.domain.dtos.GeneralResponse;
+import org.happybaras.taller3.domain.dtos.UserLoginDTO;
+import org.happybaras.taller3.domain.dtos.UserRegisterDTO;
+import org.happybaras.taller3.domain.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+    //@Autowired
+    //private UserService userService;
+
+    @PostMapping("/login")
+    public ResponseEntity<GeneralResponse> login(@ModelAttribute @Valid UserLoginDTO info) {
+        // Token token = userService.registerToken(user);
+        // return new ResponseEntity<>(new TokenDTO(token), HttpStatus.OK);
+        return GeneralResponse.builder().getResponse(); // Esto hay que quitarlo después
+    }
 }
---
+++ Taller3/src/main/java/org/happybaras/taller3/domain/dtos/UserLoginDTO.java
@@ -0,0 +1,16 @@
+package org.happybaras.taller3.domain.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginDTO {
+    @NotBlank
+    private String identifier;
+    @NotBlank
+    private String password;
+}
